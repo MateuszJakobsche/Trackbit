1774a6f878147f86e6b5f9745c14375f85db5804
Chmiel_Michał <92634372+Kolixon@users.noreply.github.com>
2021-11-27 13:23:26 +0100

    Kod JavaScript - Kalendarz
    Kod JS do bazowej wersji kalendarza.

--- script.js
+++ script.js
@@ -1,159 +1,256 @@
-(function() {
-    angular
-      .module('calendarApp', ['ngAnimate'])
-      .controller('calendarController', calendarController);
-  
-    function calendarController($scope) {
-      var vm = this,
-        now = new Date(),
-        months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        jan = daysInMonth(1, now.getFullYear()),
-        feb = daysInMonth(2, now.getFullYear()),
-        mar = daysInMonth(3, now.getFullYear()),
-        apr = daysInMonth(4, now.getFullYear()),
-        may = daysInMonth(5, now.getFullYear()),
-        jun = daysInMonth(6, now.getFullYear()),
-        jul = daysInMonth(7, now.getFullYear()),
-        aug = daysInMonth(8, now.getFullYear()),
-        sep = daysInMonth(9, now.getFullYear()),
-        oct = daysInMonth(10, now.getFullYear()),
-        nov = daysInMonth(11, now.getFullYear()),
-        dec = daysInMonth(12, now.getFullYear()),
-        monthRef = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec],
-        month = now.getMonth(),
-        monthDay = monthRef[now.getMonth()],
-        n = now.getDate(),
-        uidi,
-        uidm,
-        uid;
-  
-      vm.id = now.getDate().toString() + now.getMonth().toString();
-      vm.dataId;
-      vm.events = [];
-      vm.description;
-      vm.type = 'Social';
-      vm.month = months[month];
-      vm.next = next;
-      vm.prev = prev;
-      vm.add = add;
-  
+//global variables
+var monthEl = $(".c-main");
+var dataCel = $(".c-cal__cel");
+var dateObj = new Date();
+var month = dateObj.getUTCMonth() + 1;
+var day = dateObj.getUTCDate();
+var year = dateObj.getUTCFullYear();
+var monthText = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+var indexMonth = month;
+var todayBtn = $(".c-today__btn");
+var addBtn = $(".js-event__add");
+var saveBtn = $(".js-event__save");
+var closeBtn = $(".js-event__close");
+var winCreator = $(".js-event__creator");
+var inputDate = $(this).data();
+today = year + "-" + month + "-" + day;
 
-      function placeIt() {
-        if (month === 0) {
-          $(".date_item").first().css({
-            'margin-left': '200px'
-          })
-        } else if (month === 1) {
-          $("date_item").first().css({
-            'margin-left': '0px'
-          })
-        } else if (month === 2) {
-          $(".date_item").first().css({
-            'margin-left': '150px'
-          })
-        } else if (month === 3) {
-          $(".date_item").first().css({
-            'margin-left': '300px'
-          })
-        } else if (month === 4) {
-          $(".date_item").first().css({
-            'margin-left': '400px'
-          })
-        } else if (month === 5) {
-          $(".date_item").first().css({
-            'margin-left': '200px'
-          })
-        } else if (month === 6) {
-          $(".date_item").first().css({
-            'margin-left': '300px'
-          })
-        } else if (month === 7) {
-          $(".date_item").first().css({
-            'margin-left': '100px'
-          })
-        } else if (month === 8) {
-          $(".date_item").first().css({
-            'margin-left': '250px'
-          })
-        } else if (month === 9) {
-          $(".date_item").first().css({
-            'margin-left': '350px'
-          })
-        } else if (month === 10) {
-          $(".date_item").first().css({
-            'margin-left': '150px'
-          })
-        } else if (month === 11) {
-          $(".date_item").first().css({
-            'margin-left': '250px'
-          })
-        }
-      }
-  
-      function presentDay() {
-        $(".date_item").eq(n - 1).addClass("present");
-      }
 
-      function showDays(days) {
-        for (var i = 1; i < days; i++) {
-          var uidi = i;
-          var uidm = month;
-          var uid = uidi.toString() + uidm.toString();
-          $(".dates").append("<div class='date_item' data='" + uid + "'>" + i + "</div>");
-        }
-      }
-  
-      function daysInMonth(month, year) {
-        return new Date(year, month, 0).getDate() + 1;
-      }
-  
-      function next() {
-        if (month < 11) {
-          month++;
-          $(".dates").html('');
-          vm.month = months[month];
-          monthDay = monthRef[month];
-          showDays(monthDay);
-          placeIt();
-        }
-      }
-  
-      function prev() {
-        if (month === 0) {
-          return false
-        } else {
-          month--;
-          $(".dates").html('');
-          vm.month = months[month];
-          monthDay = monthRef[month];
-          showDays(monthDay);
-          placeIt();
-        }
-      }
-  
-      function add() {
-        vm.events.push({
-          id: vm.id,
-          description: vm.description,
-          type: vm.type
-        });
-  
-        vm.description = "";
-      }
-  
-      $(".dates").on("click", ".date_item", function() {
-        vm.id = $(this).attr('data');
-        vm.dataId = $(this).attr('data');
-        $(this).addClass("present").siblings().removeClass("present");
-        $scope.$apply();
-      });
-  
-      showDays(monthDay);
-  
-      presentDay();
-  
-      placeIt();
-  
+// ------ set default events -------
+function defaultEvents(dataDay,dataName,dataNotes,classTag){
+  var date = $('*[data-day='+dataDay+']');
+  date.attr("data-name", dataName);
+  date.attr("data-notes", dataNotes);
+  date.addClass("event");
+  date.addClass("event--" + classTag);
+}
+
+defaultEvents(today, 'YEAH!','Today is your day','important');
+defaultEvents('2017-12-25', 'MERRY CHRISTMAS','A lot of gift!!!!','festivity');
+defaultEvents('2017-05-04', "LUCA'S BIRTHDAY",'Another gifts...?','birthday');
+defaultEvents('2017-03-03', "MY LADY'S BIRTHDAY",'A lot of money to spent!!!!','birthday');
+
+
+// ------ functions control -------
+
+//button of the current day
+todayBtn.on("click", function() {
+  if (month < indexMonth) {
+    var step = indexMonth % month;
+    movePrev(step, true);
+  } else if (month > indexMonth) {
+    var step = month - indexMonth;
+    moveNext(step, true);
+  }
+});
+
+//higlight the cel of current day
+dataCel.each(function() {
+  if ($(this).data("day") === today) {
+    $(this).addClass("isToday");
+    fillEventSidebar($(this));
+  }
+});
+
+//window event creator
+addBtn.on("click", function() {
+  winCreator.addClass("isVisible");
+  $("body").addClass("overlay");
+  dataCel.each(function() {
+    if ($(this).hasClass("isSelected")) {
+      today = $(this).data("day");
+      document.querySelector('input[type="date"]').value = today;
+    } else {
+      document.querySelector('input[type="date"]').value = today;
     }
+  });
+});
+closeBtn.on("click", function() {
+  winCreator.removeClass("isVisible");
+  $("body").removeClass("overlay");
+});
+saveBtn.on("click", function() {
+  var inputName = $("input[name=name]").val();
+  var inputDate = $("input[name=date]").val();
+  var inputNotes = $("textarea[name=notes]").val();
+  var inputTag = $("select[name=tags]")
+    .find(":selected")
+    .text();
+
+  dataCel.each(function() {
+    if ($(this).data("day") === inputDate) {
+      if (inputName != null) {
+        $(this).attr("data-name", inputName);
+      }
+      if (inputNotes != null) {
+        $(this).attr("data-notes", inputNotes);
+      }
+      $(this).addClass("event");
+      if (inputTag != null) {
+        $(this).addClass("event--" + inputTag);
+      }
+      fillEventSidebar($(this));
+    }
+  });
+
+  winCreator.removeClass("isVisible");
+  $("body").removeClass("overlay");
+  $("#addEvent")[0].reset();
+});
+
+//fill sidebar event info
+function fillEventSidebar(self) {
+  $(".c-aside__event").remove();
+  var thisName = self.attr("data-name");
+  var thisNotes = self.attr("data-notes");
+  var thisImportant = self.hasClass("event--important");
+  var thisBirthday = self.hasClass("event--birthday");
+  var thisFestivity = self.hasClass("event--festivity");
+  var thisEvent = self.hasClass("event");
   
-  })();
+  switch (true) {
+    case thisImportant:
+      $(".c-aside__eventList").append(
+        "<p class='c-aside__event c-aside__event--important'>" +
+        thisName +
+        " <span> • " +
+        thisNotes +
+        "</span></p>"
+      );
+      break;
+    case thisBirthday:
+      $(".c-aside__eventList").append(
+        "<p class='c-aside__event c-aside__event--birthday'>" +
+        thisName +
+        " <span> • " +
+        thisNotes +
+        "</span></p>"
+      );
+      break;
+    case thisFestivity:
+      $(".c-aside__eventList").append(
+        "<p class='c-aside__event c-aside__event--festivity'>" +
+        thisName +
+        " <span> • " +
+        thisNotes +
+        "</span></p>"
+      );
+      break;
+    case thisEvent:
+      $(".c-aside__eventList").append(
+        "<p class='c-aside__event'>" +
+        thisName +
+        " <span> • " +
+        thisNotes +
+        "</span></p>"
+      );
+      break;
+   }
+};
+dataCel.on("click", function() {
+  var thisEl = $(this);
+  var thisDay = $(this)
+  .attr("data-day")
+  .slice(8);
+  var thisMonth = $(this)
+  .attr("data-day")
+  .slice(5, 7);
+
+  fillEventSidebar($(this));
+
+  $(".c-aside__num").text(thisDay);
+  $(".c-aside__month").text(monthText[thisMonth - 1]);
+
+  dataCel.removeClass("isSelected");
+  thisEl.addClass("isSelected");
+
+});
+
+//function for move the months
+function moveNext(fakeClick, indexNext) {
+  for (var i = 0; i < fakeClick; i++) {
+    $(".c-main").css({
+      left: "-=100%"
+    });
+    $(".c-paginator__month").css({
+      left: "-=100%"
+    });
+    switch (true) {
+      case indexNext:
+        indexMonth += 1;
+        break;
+    }
+  }
+}
+function movePrev(fakeClick, indexPrev) {
+  for (var i = 0; i < fakeClick; i++) {
+    $(".c-main").css({
+      left: "+=100%"
+    });
+    $(".c-paginator__month").css({
+      left: "+=100%"
+    });
+    switch (true) {
+      case indexPrev:
+        indexMonth -= 1;
+        break;
+    }
+  }
+}
+
+//months paginator
+function buttonsPaginator(buttonId, mainClass, monthClass, next, prev) {
+  switch (true) {
+    case next:
+      $(buttonId).on("click", function() {
+        if (indexMonth >= 2) {
+          $(mainClass).css({
+            left: "+=100%"
+          });
+          $(monthClass).css({
+            left: "+=100%"
+          });
+          indexMonth -= 1;
+        }
+        return indexMonth;
+      });
+      break;
+    case prev:
+      $(buttonId).on("click", function() {
+        if (indexMonth <= 11) {
+          $(mainClass).css({
+            left: "-=100%"
+          });
+          $(monthClass).css({
+            left: "-=100%"
+          });
+          indexMonth += 1;
+        }
+        return indexMonth;
+      });
+      break;
+  }
+}
+
+buttonsPaginator("#next", monthEl, ".c-paginator__month", false, true);
+buttonsPaginator("#prev", monthEl, ".c-paginator__month", true, false);
+
+//launch function to set the current month
+moveNext(indexMonth - 1, false);
+
+//fill the sidebar with current day
+$(".c-aside__num").text(day);
+$(".c-aside__month").text(monthText[month - 1]);
