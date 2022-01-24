49b42f6ddd69320d5e848ef5f66abe1256107033
Chmiel_Michał <92634372+Kolixon@users.noreply.github.com>
2022-01-24 14:23:54 +0100

    TDD test jednostkowy, testowanie metody

--- habitList.test.js
+++ habitList.test.js
@@ -4,9 +4,21 @@ test('habitList is created', () => {
     localStorage.setItem("habitlist", "Sleep\nExercise\nDiet\nCats");
 
     let testHabitList = new habitList();
-
     expect(testHabitList.ListOfHabits[0]).toEqual("Sleep");
+    
+
 });
+
+
+
+test('Does getInstance call constructor', () => {
+    let testHabitList1 = new habitList();
+    let testHabitList2 = habitList.getInstance();
+
+    expect(testHabitList1).toEqual(testHabitList2);
+    expect(testHabitList1).toStrictEqual(testHabitList2);
+    expect(testHabitList1).toBeInstanceOf(habitList);
+})
 
 test('Are habitList the same', () => {
     let testHabitList1 = habitList.getInstance();
