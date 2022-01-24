3fcd8d8423e19a628b71e16938ffc2fb1595a138
MateuszJakobsche <laaunair@protonmail.com>
2022-01-24 14:16:39 +0100

    testowanie TDD; test jednostkowy metod w klasie habitList

--- habitList.test.js
+++ habitList.test.js
@@ -1,1 +1,17 @@
 const habitList = require('./habitList.js');
+
+test('habitList is created', () => {
+    localStorage.setItem("habitlist", "Sleep\nExercise\nDiet\nCats");
+
+    let testHabitList = new habitList();
+
+    expect(testHabitList.ListOfHabits[0]).toEqual("Sleep");
+});
+
+test('Are habitList the same', () => {
+    let testHabitList1 = habitList.getInstance();
+    let testHabitList2 = habitList.getInstance();
+
+    expect(testHabitList1).toEqual(testHabitList2);
+    expect(testHabitList1).toStrictEqual(testHabitList2);
+})
