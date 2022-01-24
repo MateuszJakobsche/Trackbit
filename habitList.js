c82e5dc3868db4c665dbc1498018108e3687b62f
Chmiel_Michał <92634372+Kolixon@users.noreply.github.com>
2022-01-24 14:53:47 +0100

    Zmienianie habitów

--- habitList.js
+++ habitList.js
@@ -16,5 +16,19 @@ class habitList {
     return habitList.instance;
   }
 }
+localStorage.setItem("habitlist", "Sleep\nExercise\nDiet\nCats");
+if (localStorage.getItem("currenthabit") === null) {
+      localStorage.setItem("currenthabit", habitList.getInstance().ListOfHabits[0]);
+}
+ function setHabit(i){
+   localStorage.setItem('currenthabit', habitList.getInstance().ListOfHabits[i]); 
+      location.reload();
+      }
+            let selectedHabit = localStorage.getItem("currenthabit");
+
+  for (let i = 0; i < habitList.getInstance().ListOfHabits.length; i++ ){
+     var thisHabit = (habitList.getInstance().ListOfHabits)[i];
+     document.write("<li><button onClick='setHabit("+i+")'>" + thisHabit + "</button></li>");
+     }
 
   module.exports = habitList;
